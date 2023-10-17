6ed0cd0eca85da29528f57c298be1fa621110410
guymorganb <guymorganb@gmail.com>
2023-10-17 15:05:12 -0500

    pointed dynamodb from local to its regional servers

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,6 +6,23 @@ const Home = () => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [thoughts, setThoughts] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try { // It's a good idea to use a try...catch block in case the web service call doesn't work.
+        const res = await fetch('/api/users');
+        const jsonData = await res.json();
+        const _data = jsonData.sort((a, b) => // Because the database call uses the scan method, we must sort the data ourselves, which we accomplish by using the sort method. We could also apply the sort operation in the server to offload processing to the cloud instead of the client.
+          a.createdAt < b.createdAt ? 1 : -1,
+        );
+        setThoughts([..._data]);
+        setIsLoaded(true);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <main>
       <div className="flex-row justify-space-between">
